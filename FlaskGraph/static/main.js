433390e5f97c8dcdc7afe55be118305ce832d797
networkingdvo <oeveren@gmail.com>
2018-10-04 09:54:01 +0200

    Fixed order by on mainpage, added main.js, added bootstrap 4 and jquery

---
+++ FlaskGraph/static/main.js
@@ -0,0 +1,8 @@
+// (C) Copyright 2018 Hewlett Packard Enterprise Development LP.
+
+function setHidden(buttonValue)
+{
+    var element = document.getElementById("action");
+    element.value = buttonValue
+    return true;
+}
