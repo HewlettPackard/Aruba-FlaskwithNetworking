8229bcca0763dff461e3ef83fea1fcd249e90b59
networkingdvo <oeveren@gmail.com>
2020-04-03 10:42:30 +0200

    Delete main.js

--- FlaskGraph/static/main.js
+++
@@ -1,8 +0,0 @@
-// (C) Copyright 2018 Hewlett Packard Enterprise Development LP.
-
-function setHidden(buttonValue)
-{
-    var element = document.getElementById("action");
-    element.value = buttonValue
-    return true;
-}
